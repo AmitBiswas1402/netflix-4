4bf396a96ae6b443fe16c92c5e522a2f7148e257
AmitBiswas1402 <amit.142biswas@gmail.com>
2024-11-28 21:48:06 +0530

    trying to make with vite

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDmITQ_XvAWOea0htqJvx6oqAkYJ4CcJUM",
-    authDomain: "netflix-7d952.firebaseapp.com",
-    projectId: "netflix-7d952",
-    storageBucket: "netflix-7d952.firebasestorage.app",
-    messagingSenderId: "421129396965",
-    appId: "1:421129396965:web:1aa295fba72f33256fea0c"
-  };
